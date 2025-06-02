a004f45c7ae0dab0e5de58907fddcdc635e11f72
akshatnepalia <masterakshat2003@gmail.com>
2025-06-02 10:46:06 +0000

    datatypes_summary.js

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,24 @@
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+
+let userEmail;
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id==anotherId);
+
+const bigNumber=23455677899976n
+const heros=["shaktiman","naagraj","doga"];
+let myObj={
+    name:"akshat",
+    age:22,
+}
+
+function myFunction=function(){
+    console.log("hello world");;
+    
+}
+console.log(typeof bigNumber);
