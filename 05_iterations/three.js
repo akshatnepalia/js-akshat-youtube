8fff4dcc6a03f824ea4d3a2098f7991379366731
akshatnepalia <masterakshat2003@gmail.com>
2025-06-23 05:18:51 +0000

    container congif done

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -6,12 +6,12 @@
 const arr = [1, 2, 3, 4, 5]
 
 for (const num of arr) {
-    //console.log(num);
+    console.log(num);
 }
 
 const greetings = "Hello world!"
 for (const greet of greetings) {
-    //console.log(`Each char is ${greet}`)
+    console.log(`Each char is ${greet}`)
 }
 
 // Maps
